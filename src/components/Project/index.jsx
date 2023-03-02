3164b9fc36e24593d4b9dac38b469db63fece216
Agia <agia@pm.me>
2023-03-02 22:14:19 +0000

    Added initial render content to Project, and dummy data to projects.json for building/testing

---
+++ src/components/Project/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+
+
+const Project = (props) => {
+    return (
+        <div className="projectCard" key={props.id}>
+            <div className="cardFlip">
+                <div className="cardFront">
+                    <h3>{props.name}</h3>
+                    <img src={props.image} alt={props.name} />
+                </div>
+                <div className="cardBack">
+                    <p>{props.description}</p>
+                    <ul>
+                    {
+                        props.skills.map(skill => {
+                            return (
+                                <li className="skill-list-item">{skill}</li> 
+                            )
+                        })
+                    }
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Project;
