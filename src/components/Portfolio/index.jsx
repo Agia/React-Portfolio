4ae6073722b83e014e7a126b3aaff79aca131e91
Agia <agia@pm.me>
2023-03-02 22:20:44 +0000

    Add initial render content to Portfolio, using data from projects.json

---
+++ src/components/Portfolio/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Project from "../Project";
+import projects from "../../data/projects.json";
+import "./style";
+
+
+class Portfolio extends React.Component {
+    state = {projects};
+
+    render() {
+        return (
+            <div id="portfolio">
+                <h2>Portfolio</h2>
+                <p>Below is a collection of things I've built so far.</p>
+                {
+                    this.state.projects.map(project => {
+                        return (
+                            <Project
+                            name={project.name}
+                            image={project.image}
+                            description={project.description}
+                            skills={project.skills}
+                            key={project.id}
+                            />
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+
+export default Portfolio;
