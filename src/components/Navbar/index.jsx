ec5614b7503b9387fe6a0b8848371a5f4976706d
Agia <agia@pm.me>
2023-03-04 17:17:05 +0000

    Changes to Navbar; open from side at smaller screen sizes, corrected z-index issue with nav and portfolio components and other minor tweaks

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -9,80 +9,52 @@ const Navbar = () => {
 
     const [navExpanded, setNavExpanded] = useState(false);
 
+  const handleNavExpanded = () => {
+    setNavExpanded(!navExpanded);
+    if (this.target.className === "button button-menu fas fa-bars") {
+        this.target.className = "button button-menu fas fa-cross";
+    } else {
+        this.target.className = "button button-menu fas fa-bars";
+    }
+  }
+
   return (
-    <nav>
-      <div className="logo">
-        <Link to="/" className="logo-link">
-          <img src={logo} alt="<AA>" className="logo-img" />
-          <span className="logo-name">Alyssa Argento</span>
-        </Link>
+    <nav className="navbar">
+        <div className="container">
+            <div className="logo">
+                <Link to="/" className="logo-link">
+                    <img src={logo} alt="<AA>" className="logo-img" />
+                    <span className="logo-name">Alyssa Argento</span>
+                </Link>
+            </div>
+
+        <button className="button button-menu fas fa-bars"
+                 onClick={handleNavExpanded}>
+            <span className="button-label">Menu</span>
+        </button>
+
+        <div className={`nav-list  ${navExpanded && 'active'}`}>
+          <ul>
+            <li>
+              <NavLink to="/React-Portfolio/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/React-Portfolio/skills">Skills</NavLink>
+            </li>
+            <li>
+              <NavLink to="/React-Portfolio/portfolio">Portfolio</NavLink>
+            </li>
+            <li>
+              <NavLink to="React-Portfolio/testimonials">Testimonials</NavLink>
+            </li>
+            <li>
+              <NavLink to="React-Portfolio/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-
-      <button className="button button-menu"
-                onClick={() => {
-                    setNavExpanded(!navExpanded);
-                }}>
-        Menu
-      </button>
-
-      <ul className={navExpanded ? "nav-list expanded" : "nav-list"}
-      >
-        <li className="nav-item">
-          <NavLink
-            to="/React-Portfolio/"
-            end
-            className={({ isActive }) =>
-              isActive ? "nav-link active" : "nav-link"
-            }
-          >
-            Home
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="/React-Portfolio/skills"
-            className={({ isActive }) =>
-              isActive ? "nav-link active" : "nav-link"
-            }
-          >
-            Skills
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="/React-Portfolio/portfolio"
-            className={({ isActive }) =>
-              isActive ? "nav-link active" : "nav-link"
-            }
-          >
-            Portfolio
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="testimonials"
-            end
-            className={({ isActive }) =>
-              isActive ? "nav-link active" : "nav-link"
-            }
-          >
-            Testimonials
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="contact"
-            end
-            className={({ isActive }) =>
-              isActive ? "nav-link active" : "nav-link"
-            }
-          >
-            Contact
-          </NavLink>
-        </li>
-      </ul>
     </nav>
-  );
-};
+  )
+}
 
 export default Navbar;
